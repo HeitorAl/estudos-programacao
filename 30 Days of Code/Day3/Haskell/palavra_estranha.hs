286fe69b466a58b172a67595b411e125cda18ae5
Heitor Bittencourt <heitoral79@outlook.com>
2023-09-07 21:52:22 -0300

    Codigo do dia 4 em Haskell

---
+++ 30 Days of Code/Day3/Haskell/palavra_estranha.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DuplicateRecordFields, FlexibleInstances, UndecidableInstances #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Data.Text
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+
+
+lstrip = Data.Text.unpack . Data.Text.stripStart . Data.Text.pack
+rstrip = Data.Text.unpack . Data.Text.stripEnd . Data.Text.pack
+
+main :: IO()
+main = do
+    nTemp <- getLine
+    let n = read $ lstrip $ rstrip nTemp :: Int
+    if (odd n || (n >= 6 && n <= 20)) then putStrLn "Weird" else putStrLn "Not Weird"
