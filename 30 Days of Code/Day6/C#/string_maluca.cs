2b16768d64d34c38f8ecb679d05a984df41f8bac
Heitor Bittencourt <heitoral79@outlook.com>
2023-09-10 22:23:23 -0300

    Código em C# do dia 7

---
+++ 30 Days of Code/Day6/C#/string_maluca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+    int n = int.Parse(Console.ReadLine());
+
+    while (n > 0) {
+        string palavra = Console.ReadLine(), impar = "", par = "";
+        int tamanho = palavra.Length;
+        for (int i = 0; i < tamanho; i++) {
+            if (i % 2 == 0) {
+                par += palavra[i];
+            }
+            if (i % 2 != 0) {
+                impar += palavra[i];
+            }
+        }
+        Console.WriteLine($"{par} {impar}");
+        n--;
+    }
+    }
+}
