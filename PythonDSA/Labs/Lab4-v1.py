1b0265fab2f38e3172b8a09e30a6ace15d6c9ae4
HeitorAl <heitoral79@outlook.com>
2024-12-01 11:31:39 -0300

    Jogo da forca com POO

--- PythonDSA/Labs/Lab4-v1.py
+++ PythonDSA/Labs/Lab4-v1.py
@@ -3,6 +3,7 @@
 
 # Import
 import random
+from os import system
 
 # Board (tabuleiro)
 board = ['''
@@ -39,7 +40,7 @@ O   |
 /|   |
      |
      |
-=========''', '''
+=========''', r'''
 
  +---+
  |   |
@@ -47,7 +48,7 @@ O   |
 /|\  |
      |
      |
-=========''', '''
+=========''', r'''
 
  +---+
  |   |
@@ -55,7 +56,7 @@ O   |
 /|\  |
 /    |
      |
-=========''', '''
+=========''', r'''
 
  +---+
  |   |
@@ -70,14 +71,67 @@ O   |
 class Hangman:
 
 	# Método Construtor
+    def __init__(self, palavras):
+        self.palavra = random.choice(palavras).lower()
+        self.letras_corretas = ["_" for _ in self.palavra]
+        self.letras_erradas = []
+        self.tentativas = 0
+        self.max_tentativas = 6
+        self.hangman = board
+
+    # Método para limpar a tela
+    def limpa_tela(self):
+        _ = system("cls")
 
 	# Método para adivinhar a letra
-	
+    def guess(self):
+        letra = input("Digite uma letra: ").lower()
+        if letra in self.letras_corretas or letra in self.letras_erradas:
+            print("\nVocê já tentou essa letra!\n")
+            return False
+
+        if letra in self.palavra:
+            for i, l in enumerate(self.palavra):
+                if letra == l:
+                    self.letras_corretas[i] = letra
+
+        else:
+            print(f"Letra {letra} não está na palavra")
+            self.letras_erradas.append(letra)
+            self.tentativas += 1
+        return True
+
 	# Método para verificar se o jogo terminou
+    def hangman_over(self):
+        return self.hangman_won() or self.tentativas == self.max_tentativas
 		
 	# Método para verificar se o jogador venceu
-		
-	# Método para não mostrar a letra no board
+    def hangman_won(self):
+        if "_" not in self.letras_corretas:
+            return True
 		
 	# Método para checar o status do game e imprimir o board na tela
+    def hangman_status(self):
+        print('\n' + self.hangman[self.tentativas] + "\n")
+        print("Palavra: " + " ".join(self.letras_corretas) + "\n")
+        print("Letras erradas:\n" + "\n".join(self.letras_erradas) + "\n")
 
+lista_palavras = ['casa', 'sol', 'lua', 'gato', 'cao', 'bola',
+                  'pe', 'mao', 'agua', 'ar', 'arvore', 'nuvem',
+                  'janela', 'porta', 'lapis', 'livro', 'computador',
+                  'bicicleta', 'montanha', 'oceano', 'quiabo',
+                  'xicara', 'xadrez', 'bussola', 'girassol', 'melancia',
+                  'abacaxi', 'escafandro', 'hipopotamo', 'psicologia']
+jogo = Hangman(lista_palavras)
+jogo.limpa_tela()
+
+while not jogo.hangman_over():
+    jogo.hangman_status()
+    jogo.guess()
+
+jogo.hangman_status()
+if jogo.hangman_won():
+    print("\nParabéns você venceu!")
+else:
+    print("\nGame over! Você perdeu!")
+    print(f"A palavra era: {jogo.palavra}")
